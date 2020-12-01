f6e308e2d5f007d5a5af5f816710a7f5283b23ed
Healing Storm <vincentpy0902@gmail.com>
2020-12-01 16:25:02 +0100

    Début Tire du swap sur tout les axes du personnage

---
+++ Coco_Unchained/Assets/Script/Player.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [Header("Movement Settings")]
+    // Movespeed player value
+    public float moveSpeed = 0.1f;
+    private Vector3 _direction;
+
+    public GameObject SwapProjectile;
+    public GameObject FirePointLeft;
+    public GameObject FirePointRight;
+    public GameObject FirePointUp;
+    public GameObject FirePointDown;
+
+    public bool Horizontal;
+    public bool Vertical;
+
+    private void Update()
+    {
+
+        // Get move x and z input direction
+        _direction = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0f);
+
+        if (_direction.x > 0 && Input.GetKeyDown(KeyCode.Space))
+        {
+            //right
+            Instantiate(SwapProjectile, FirePointRight.transform.position, FirePointRight.transform.rotation);
+        }
+
+        if (_direction.x < 0 && Input.GetKeyDown(KeyCode.Space))
+        {
+            //left
+            Instantiate(SwapProjectile, FirePointLeft.transform.position, FirePointLeft.transform.rotation);
+        }
+
+        if (_direction.y < 0 && _direction.x == 0 && Input.GetKeyDown(KeyCode.Space))
+        {
+            //down
+            Instantiate(SwapProjectile, FirePointDown.transform.position, FirePointDown.transform.rotation);
+        }
+
+        if (_direction.y > 0 && _direction.x == 0 && Input.GetKeyDown(KeyCode.Space))
+        {
+            //up
+            Instantiate(SwapProjectile, FirePointUp.transform.position, FirePointUp.transform.rotation);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // Call the PlayerMove function
+        PlayerMove();
+    }
+
+    private void PlayerMove()
+    {
+        // Set the movement X and Z with the movespeed
+        var targetMovement = new Vector3(_direction.x, _direction.y, 0f) * moveSpeed;
+        transform.Translate(new Vector3(targetMovement.x, targetMovement.y, 0f));
+    }
+
+    
+
+}
---
+++ Coco_Unchained/Assets/Script/SwapProjectile.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwapProjectile : MonoBehaviour
+{
+    public Player PlayerScript;
+    public float speed = 20f;
+    public Rigidbody2D rb;
+    void Awake()
+    {
+        rb.velocity = transform.right * speed;
+        Destroy(gameObject, 5f);
+    }
+}
