4111ccdc447641012b28a4b280d0c7ec852cbc9f
Healing Storm <vincentpy0902@gmail.com>
2020-12-15 16:22:09 +0100

    Torches et portes fonctionnelles

--- Coco_Unchained/Assets/Script/DoorScript.cs
+++ Coco_Unchained/Assets/Script/DoorScript.cs
@@ -5,35 +5,20 @@ using System.Linq;
 
 public class DoorScript : MonoBehaviour
 {
+    [Header("Mettre trois torches pour la dernière porte du LD")]
+    [Header("Mettre une seule Torche pour les deux premières portes du LD")]
+    [Header("Torches qui appartiennent à la porte")]
     public GameObject[] TorchesToActivate;
-    private bool[] TorchesActivated;
+    
+    [HideInInspector]
+    public bool[] TorchesActivated;
 
     private void Start()
     {
-        Debug.Log(TorchesToActivate.Length);
         TorchesActivated = new bool[TorchesToActivate.Length];
         for (int i = 0; i < TorchesActivated.Length; i++)
         {
             TorchesActivated[i] = false;
-        }
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        for (int i = 0; i < TorchesToActivate.Length; i++)
-        {
-            if(collision.gameObject == TorchesToActivate[i])
-            {
-                TorchesActivated[i] = true;
-                Destroy(TorchesToActivate[i]);
-
-                for (int y = 0; y < TorchesActivated.Length; y++)
-                {
-                    if (TorchesActivated.All(a => !a))
-                    {
-                        Destroy(gameObject);
-                    }
-                }
-            }
         }
     }
 }
